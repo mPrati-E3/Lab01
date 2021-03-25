24455f42b720988cf37051e8e040273ec78f58fe
MPrati <MPrati@HP-250-G8-MP.homenet.telecomitalia.it>
2021-03-25 11:50:06 +0100

    Prima versione modello

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,28 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Parole {
+	
+	private List<String> elenco;
 		
 	public Parole() {
-		//TODO
+		this.elenco= new ArrayList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		this.elenco.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		Collections.sort(this.elenco);
+		return this.elenco;
 	}
 	
 	public void reset() {
-		// TODO
+		this.elenco= new ArrayList<String>();
 	}
 
 }
